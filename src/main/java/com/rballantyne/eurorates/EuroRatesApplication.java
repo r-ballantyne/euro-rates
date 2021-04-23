69ec643d715b3d9ebed17355ff95fca658e16fe6
Ruari Ballantyne <ruariball@outlook.com>
2021-04-23 21:16:13 +0100

    Initial commit

---
+++ src/main/java/com/rballantyne/eurorates/EuroRatesApplication.java
@@ -0,0 +1,13 @@
+package com.rballantyne.eurorates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroRatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroRatesApplication.class, args);
+	}
+
+}
