92d063ad73107b1660cc1c18ecf4fde29ad3c5a3
Ruari Ballantyne <ruariball@outlook.com>
2021-04-23 21:56:55 +0100

    Set up endpoints in controller

--- src/main/java/com/rballantyne/eurorates/controller/EuroRatesController.java
+++ src/main/java/com/rballantyne/eurorates/controller/EuroRatesController.java
@@ -1,8 +1,55 @@
 package com.rballantyne.eurorates.controller;
 
-import org.springframework.stereotype.Controller;
+import java.time.LocalDate;
 
-@Controller
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rballantyne.eurorates.model.ExchangeRate;
+import com.rballantyne.eurorates.model.ReferenceDay;
+
+@RestController
 public class EuroRatesController {
 
+	private static final Logger logger = LoggerFactory.getLogger(EuroRatesController.class);
+
+	@GetMapping()
+	public ReferenceDay getReferenceDataForDay(LocalDate date) {
+
+		logger.info("Received request at getReferenceDataForDay: date={}");
+
+		return null;
+	}
+
+	@GetMapping()
+	public long getExchangedAmountForDay(LocalDate date, String sourceCurrency, String targetCurrency) {
+
+		logger.info("Received request at getExchangedAmountForDay: date={}, sourceCurrency={}, targetCurrency={}", date,
+				sourceCurrency, targetCurrency);
+
+		return 0l;
+	}
+
+	@GetMapping()
+	public ExchangeRate getCurrencyHighestExchangeRateForPeriod(LocalDate startDate, LocalDate endDate,
+			String currency) {
+
+		logger.info(
+				"Received request at getCurrencyHighestExchangeRateForPeriod: startDate={}, endDate={}, currency={}",
+				startDate, endDate, currency);
+
+		return null;
+	}
+
+	@GetMapping()
+	public long getCurrencyAverageExchangeRateForPeriod(LocalDate startDate, LocalDate endDate, String currency) {
+
+		logger.info(
+				"Received request at getCurrencyAverageExchangeRateForPeriod: startDate={}, endDate={}, currency={}",
+				startDate, endDate, currency);
+
+		return 0l;
+	}
 }
